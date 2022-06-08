48d2c1f135386459a619d0c32b261022073514a6
anuraga091 <anuraga091@gmail.com>
2022-06-08 10:43:04 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { fabric } from "fabric";
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    var canvas = new fabric.Canvas('canvas');
+    canvas.add( new fabric.Rect({
+    left: 100,
+    top: 200,
+    fill: 'red',
+    width: 50,
+    height: 50,
+  }));
+
+  canvas.item(0).set({
+    centeredScaling : true,
+    centeredRotation: true
+  })
+  const [txt, setTxt] = useState("False");
+  const [xAxis, setXAxis] = useState(0);
+  const [yAxis, setYAxis] = useState(0);
+  
+  canvas.on('mouse:down', function(e) {
+  console.log(e);
+  setTxt(<p> True </p>);
+  canvas.item(0).transparentCorners= true
+  console.log(e.target.top, e.target.left)
+
+});
+canvas.on('mouse:out', function(e) {
+  setTxt(<p>False</p>);
+
+});
+
+canvas.on('mouse:over', function(e) {
+  setTxt(<p>True</p>);
+});
+
+canvas.on('mouse:move', function(e){
+  setXAxis(e.target.left);
+  setYAxis(e.target.top)
+})
+
+
+ // make object unselectable
+
+    return (
+    <>
+      <p> Welcome to Fabric Canvas, This work is made by Anurag</p>
+      <p>Shape Clicked: {txt}</p>
+      <div>
+        <span>X- axis position : {xAxis} </span>
+        <span>Y- axis position: {yAxis} </span>
+      </div>
+      <canvas id="canvas" ></canvas>
+    
+    </>
   );
 }
-
+  
 export default App;
